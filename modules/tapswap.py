cd4ae9c852a984854f31b1cc1c9500efc268f3b8
Tong Vuu <tongvuu@gmail.com>
2024-05-29 09:46:49 +0700

    Tapswap is working now

--- modules/tapswap.py
+++ modules/tapswap.py
@@ -1,8 +1,8 @@
-from .base import basetap
+from base import basetap
 import requests
 from datetime import datetime
 import time
-
+import struct
 
 DEFAULT_HEADERS = {
     "Accept": "*/*",
@@ -27,6 +27,16 @@ DEFAULT_HEADERS = {
 
 DEFAULT_AUTH = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjU2MjQyNTgxOTQsImlhdCI6MTcxNjkwOTI0NywiZXhwIjoxNzE2OTEyODQ3fQ.IO35nSuv6-R3zgR9UB7-j6X7sjGM9wJ8Nj6ujZT91-0"
 # DEFAULT_AUTH = ""
+def to_js_float64(value):
+    """Convert a large integer to a float64 representation, simulating JavaScript precision issues."""
+    return struct.unpack('d', struct.pack('d', float(value)))[0]
+
+def js_modulo(a, b):
+    """Simulate JavaScript modulo operation for large integers with precision issues."""
+    a_js = to_js_float64(a)
+    b_js = to_js_float64(b)
+    result = (a_js * b_js) % a_js
+    return result
 
 url = "https://api.tapswap.ai/api/player/submit_taps"
 
@@ -52,13 +62,13 @@ class tapswap(basetap):
         try:
             response = requests.post(url, headers=self.headers, json=body, proxies=self.proxy)
             data = response.json()
-            
+            print(data)
             if "access_token" in data:
                 self.auth = f"Bearer {data['access_token']}"
                 self.update_header("Authorization", self.auth)
                 self.bprint("Login success")
                 self.energy = int(data['player']['energy'])
-                # self.tap()
+                self.tap(fromlogin=True)
                 return True
             self.bprint("Login failed")
         except Exception as e:
@@ -66,39 +76,41 @@ class tapswap(basetap):
         return False
 
     def update_content_id(self, now):
-        from decimal import Decimal
-        now = Decimal(now)
-        myid = Decimal(self.init_data["user"]["id"])
-        contentid = (now * myid) % myid
-        print(now)
+        myid = self.init_data["user"]["id"]
+        contentid = int(js_modulo(myid, now))
         self.update_header("Content-Id", str(contentid))
-        print(contentid)
 
-    def tap(self):
+    def tap(self, fromlogin = False):
         current_time_seconds = time.time()
         epoch_ms = int(current_time_seconds * 1000)
-        self.update_content_id(1716912946864)
+        self.update_content_id(epoch_ms)
         data = {
             "taps": self.energy,
             "time": epoch_ms 
         }
-        print(data)
         try:
             response = requests.post(url, headers=self.headers, json=data, proxies=self.proxy)
             data = response.json()
-            print(data)
             if "statusCode" in data:
-                self.bprint(f"Error: {data['message']}, try re-login")
-                # self.login()
+                if fromlogin:
+                    self.bprint(f"Error {data['message']}, stop looping since code is failed")
+                    self.stopped = True
+                else:
+                    self.bprint(f"Error: {data['message']}, try re-login")
+                    self.login()
             else:
                 self.print_balance(data['player']['shares'])
                 self.energy = 10
         except Exception as e:
             self.bprint(e)
 
-    def run(self):
-        self.wait_time = 10
-        while not self.stopped:
-            self.tap()
-            self.wait()
-            break
+
+if __name__ == "__main__":
+    obj = tapswap()
+    obj.init_data_raw = "query_id=AAGSXjtPAgAAAJJeO0_t0i5G&user=%7B%22id%22%3A5624258194%2C%22first_name%22%3A%22Evis%22%2C%22last_name%22%3A%22The%20Cat%22%2C%22username%22%3A%22rokbotsxyz%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&auth_date=1716909225&hash=109f0f7fbe349f2d9e6e563734735131a56bc9942955c5e162352d5a7bbad9fe"
+    obj.init_data = {
+        "user" : {
+            "id" : 5624258194
+        }
+    }
+    obj.tap()
